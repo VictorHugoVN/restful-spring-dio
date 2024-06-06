61328f31a4c084a813f25a89b6487c81a931c830
VictorHugoVN <victorneivadef8@gmail.com>
2024-06-06 11:55:56 -0300

    Projeto básico criado

---
+++ src/main/java/me/dio/RestfulSpringDioApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulSpringDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulSpringDioApplication.class, args);
+	}
+
+}
